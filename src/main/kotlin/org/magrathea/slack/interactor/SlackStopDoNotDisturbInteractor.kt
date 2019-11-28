1c1714f6b0be777dc9fead0f5ce3f6ea69a430b1
Marcos Felipe Eipper <marcos@eipper.com.br>
2019-11-27 22:59:44 -0300

    Remove _too much_ implicity

--- src/main/kotlin/org/magrathea/slack/interactor/SlackStopDoNotDisturbInteractor.kt
+++ src/main/kotlin/org/magrathea/slack/interactor/SlackStopDoNotDisturbInteractor.kt
@@ -1,14 +1,11 @@
 package org.magrathea.slack.interactor
 
 import com.github.seratch.jslack.api.methods.MethodsClient
+import com.github.seratch.jslack.api.methods.request.dnd.DndEndDndRequest
 
 object SlackStopDoNotDisturbInteractor {
 
     fun execute(methodsClient: MethodsClient) {
-        methodsClient.apply {
-            dndEndDnd {
-              it
-            }
-        }
+        methodsClient.dndEndDnd(DndEndDndRequest.builder().build())
     }
 }
